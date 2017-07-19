a56b6debb8d35109a0d8b0cab91a26a65f3553d2
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-18 21:55:04 -0400

    finish assignment a 2nd time :)

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -48,4 +48,12 @@ class PicturesController < ApplicationController
      end
    end
 
+   def destroy
+     @picture = Picture.find(params[:id])
+     @picture.destroy
+     redirect_to "/pictures"
+   end
+
+
+
 end
