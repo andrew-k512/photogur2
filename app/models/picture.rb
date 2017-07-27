703294adc9afddfcd9a55dc06041011829466d08
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-26 23:58:36 -0400

    add error messages to new picture form page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,19 @@
 class Picture < ApplicationRecord
 
-  validates_presence_of :artist, :url
-  validates_uniqueness_of :url
-  validates_length_of :title, :within => 3..20 
+# long form validations
 
-# validates :url, uniqueness: true
-# validates :url, presence: true
+  # validates_presence_of :artist, :url
+  # validates_uniqueness_of :url
+  # validates_length_of :title, :within => 3..20
+
+# 'sexy' validations
+
+  validates :url, uniqueness: true,
+                  length: { minimum: 2},
+                  presence: true
+  validates :artist, presence: true,
+                     length: { minimum: 2 }
+  validates :title, length: { minimum: 2 },
+                    presence: true
 
 end
