ae9dcfab92ff7769eb6c1f01abfdd28b32ef3759
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-26 22:39:44 -0400

    add several validates methods that were tested in irb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,10 @@
 class Picture < ApplicationRecord
+
+  validates_presence_of :artist, :url
+  validates_uniqueness_of :url
+  validates_length_of :title, :within => 3..20 
+
+# validates :url, uniqueness: true
+# validates :url, presence: true
+
 end
